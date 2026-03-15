9f8476dfef71d3769c92b6b91c183be88ac7d7ae
bradscorsa <163941717+bradscorsa@users.noreply.github.com>
2026-03-15 19:36:26 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+  self.skipWaiting();
 });
-self.addEventListener('fetch', (e) => {});
+
+self.addEventListener('fetch', (e) => {
+  // A basic pass-through that satisfies Android's offline capability check
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return new Response('App is offline');
+    })
+  );
+});
